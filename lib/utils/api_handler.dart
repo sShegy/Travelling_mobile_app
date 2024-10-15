039acac9949308f47d892d787465db78eb19a874
Shegy <msegan13323ri@raf.rs>
2024-10-15 18:40:59 +0200

    first commit

---
+++ lib/utils/api_handler.dart
@@ -0,0 +1,5 @@
+class BaseApi {
+  //napravite vas nalog na serpapiju i koristite vas kljuc!!
+  static var api =
+      'https://serpapi.com/search.json?engine=google&q=Italy+Destinations&api_key=YOUR_API_KEY';
+}
